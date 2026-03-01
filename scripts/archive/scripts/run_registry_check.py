02bf200d5b75bb40d18afeaf059b218f2b9b3d25
moto@example.com <moto@example.com>
2026-03-01 13:07:12 +0900

    Isolate blog repo and publish article

--- scripts/archive/scripts/run_registry_check.py
+++
@@ -1,5 +0,0 @@
-import importlib
-importlib.invalidate_caches()
-import src.services.framework.service_registry as sr
-print('service_registry imported; registered modules count =', len(sr.service_registry._modules))
-print('Sample modules:', list(sr.service_registry._modules.keys())[:20])
